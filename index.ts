e1eaca60d5ef5bd2a72ea662618191e281e1a90a
Jacob Singer <jacobleesinger@gmail.com>
2016-08-09 11:09:33 -0700

    setting up handlebars and adding view/layout

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import * as path from 'path';
+import * as ExpressHandlebars from 'express-handlebars';
 import './db';
 import './models';
 
@@ -7,6 +8,15 @@ const app = express();
 
 app.get('/', function(req, res) {
     res.send("hello world");
+});
+
+// set handlebars as view engine
+const handlebars = ExpressHandlebars.create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.get('/search', function(req, res) {
+    res.render('search');
 });
 
 const server = app.listen(8080);
